6f0ff3bd4c74b2d888cc55aa1e4e19697ab870d3
wb121017405 <121017405@qq.com>
2020-05-29 09:48:09 +0800

    新增一本我在末世有套房

--- novels/.vuepress/config.js
+++ novels/.vuepress/config.js
@@ -41,10 +41,10 @@ module.exports = {
   },
   plugins: [
     [
+      // 评论插件
       'vuepress-plugin-comment',
       {
         choosen: 'valine',
-        // options选项中的所有参数，会传给Valine的配置
         options: {
           el: '#valine-vuepress-comment',
           appId: '8aW60X6Gmv1Dy8APCuaBf2Ye-gzGzoHsz',
